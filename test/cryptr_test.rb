9ab7329b16a3121c3da43be4ff791da06b2eebac
zhuojie <zhuojie@checkr.com>
2017-10-02 14:54:19 -0700

    Add multi-decrypt support

--- test/cryptr_test.rb
+++ test/cryptr_test.rb
@@ -1,9 +1,12 @@
 require 'test_helper'
 require 'securerandom'
 
+# rubocop:disable Metrics/AbcSize
+# rubocop:disable Metrics/MethodLength
+
 class CryptrTest < Minitest::Test
   def test_it_can_decrypt_encrypt_random_data
-    100.times do
+    1000.times do
       data = SecureRandom.base64
       key = 'some_key'
       assert data == Cryptr.decrypt(key, Cryptr.encrypt(key, data))
@@ -11,10 +14,58 @@ class CryptrTest < Minitest::Test
   end
 
   def test_it_can_decrypt_encrypt_random_data_base64
-    100.times do
+    1000.times do
       data = SecureRandom.base64
       key = 'some_key'
       assert data == Cryptr.decrypt64(key, Cryptr.encrypt64(key, data))
+    end
+  end
+
+  def test_it_can_decrypt_encrypt_with_multi_keys
+    1000.times do
+      data = SecureRandom.base64
+      key1 = 'some_key_1'
+      key2 = 'some_key_2'
+      key3 = 'some_key_3'
+
+      assert Cryptr.multi_decrypt(
+        [key1, key2],
+        Cryptr.encrypt(key1, data)
+      ).include?(data)
+
+      assert Cryptr.multi_decrypt(
+        [key1, key2],
+        Cryptr.encrypt(key2, data)
+      ).include?(data)
+
+      assert !Cryptr.multi_decrypt(
+        [key1, key2],
+        Cryptr.encrypt(key3, data)
+      ).include?(data)
+    end
+  end
+
+  def test_it_can_decrypt_encrypt_with_multi_keys_base64
+    1000.times do
+      data = SecureRandom.base64
+      key1 = 'some_key_1'
+      key2 = 'some_key_2'
+      key3 = 'some_key_3'
+
+      assert Cryptr.multi_decrypt64(
+        [key1, key2],
+        Cryptr.encrypt64(key1, data)
+      ).include?(data)
+
+      assert Cryptr.multi_decrypt64(
+        [key1, key2],
+        Cryptr.encrypt64(key2, data)
+      ).include?(data)
+
+      assert !Cryptr.multi_decrypt64(
+        [key1, key2],
+        Cryptr.encrypt64(key3, data)
+      ).include?(data)
     end
   end
 end
